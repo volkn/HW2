f6214d0bad5e03f14375c9dad51f52876c27a65e
volkn <uselesspig@outlook.com>
2018-07-21 16:05:30 +0300

    done

--- 1/matrix_product.py
+++ 1/matrix_product.py
@@ -1,12 +1,21 @@
 def matrix_product(A,B):
-    result=[[0,0,0],
-            [0,0,0],
-            [0,0,0]]
-    for i in range(3):
-        for j in range(3):
-            for k in range(3):
-                result[i][j] += A[i][k] * B[k][j]
-    return result
+    rowA = len(A)
+    rowB = len(B)
+    colA = len(A[0])
+    colB = len(B[0])
+
+    if colA == rowB:
+        result=[[0,0,0],
+                [0,0,0],
+                [0,0,0]]
+
+        for i in range(rowA):
+            for j in range(colB):
+                for k in range(colA):
+                    result[i][j] += A[i][k] * B[k][j]
+        return result
+    else:
+        print("matrices cant be multiplied")
 
 matrix1 = [[1,0,0],
            [0,1,0],
@@ -16,6 +25,14 @@ matrix2 = [[1,0,0],
            [0,1,0],
            [0,0,1]]
 
+matrix3 =  [[1,2],
+            [0,1],
+            [0,0]]
+
+matrix4 =  [[1,0,0],
+            [1,0,2]]
+
 print(matrix_product(matrix1,matrix2))
+print(matrix_product(matrix3,matrix4))
 
 
