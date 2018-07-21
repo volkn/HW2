2ad19755ce197b19ff466fa668393765b1c092cb
volkn <uselesspig@outlook.com>
2018-07-22 01:33:47 +0300

    done

--- 1/matrix_product.py
+++ 1/matrix_product.py
@@ -1,3 +1,48 @@
+first_matrix_type = str(input("insert the type of your first matrix, like 3x4: "))
+if type(first_matrix_type) != type(str()):
+    print("nope, try again")
+second_matrix_type = str(input("insert the type of your second matrix, like 4x3: "))
+if type(second_matrix_type) != type(str()):
+    print("nope, try again")
+
+first = first_matrix_type.split("x")
+second = second_matrix_type.split("x")
+
+matrix_1 = []
+matrix_2 = []
+row = []
+
+if (len(first) == 2)and (len(second) == 2):
+    if (first[1] == second[0]):
+        for i in range(int(first[0])):
+            first_matrix_rows = str(input("insert row {}, elements with space between them: ".format(i)))
+            a = first_matrix_rows.split(" ")
+            if len(a) > int(first[1]):
+                print("nope")
+            else:
+                for number in a:
+                    row.append(int(number))
+                matrix_1.append(row)
+                row = []
+        print("your first matrix is: " + matrix_1)
+        for i in range(int(second[0])):
+            second_matrix_rows = str(input("insert row {}, elements with space between them: ".format(i)))
+            b = second_matrix_rows.split(" ")
+            if len(b) > int(second[1]):
+                print("nope")
+            else:
+                for number in b:
+                    row.append(int(number))
+                matrix_2.append(row)
+                row = []
+    
+        print("your second matrix is: " + matrix_2)
+    else:
+        print("first matrices column number must be equal to seconds row numbers")
+else:
+    print("matrices can be have rows and columns")
+
+
 def matrix_product(A,B):
     rowA = len(A)
     rowB = len(B)
@@ -17,22 +62,4 @@ def matrix_product(A,B):
     else:
         print("matrices cant be multiplied")
 
-matrix1 = [[1,0,0],
-           [0,1,0],
-           [0,0,1]]
-
-matrix2 = [[1,0,0],
-           [0,1,0],
-           [0,0,1]]
-
-matrix3 =  [[1,2],
-            [0,1],
-            [0,0]]
-
-matrix4 =  [[1,0,0],
-            [1,0,2]]
-
-print(matrix_product(matrix1,matrix2))
-print(matrix_product(matrix3,matrix4))
-
-
+print(matrix_product(matrix_1,matrix_2))
