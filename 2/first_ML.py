668c91e6a16fa3dd6d6b298fb036d68b804a196a
volkn <uselesspig@outlook.com>
2018-08-02 23:29:38 +0300

    i guess this is what im trying to find

---
+++ 2/first_ML.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt 
+import numpy as np 
+import matplotlib 
+from sympy import * 
+
+rng = np.random.RandomState(1)
+list_x = 5 * rng.rand(50)
+list_y = 2 * list_x - 5 + rng.randn(50)
+#print(list_x)
+#print(list_y)
+line_tuples_list = []
+
+line_y = []
+#x = Symbol('x')
+#y = Symbol('y')
+m = Symbol('m')
+n = Symbol('n')
+
+line_y = []
+y = 0
+for i in list_x:
+    y = m * i + n
+    line_y.append(y)
+    y = 0
+
+line_y = np.array(line_y)
+
+list_y = list_y - line_y
+#print(list_y)
+a = 0
+for i in list_y:
+    a += i
+print(a)
+
+#print(line_y)
+#print(line_y)
+#plt.scatter(list_x, list_y)
+#plt.show()
